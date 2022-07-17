2dd9850d2cb33408d9b70b29971bb29804d809b2
Andrew Scott Wise <andrewswise2795@gmail.com>
2022-07-17 18:57:15 +0100

    added create member functionality

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.member import Member
@@ -11,3 +12,16 @@ members_blueprint = Blueprint("members", __name__)
 def members():
     members = member_repository.select_all()
     return render_template("/members/index.html", members=members)
+
+@members_blueprint.route("/home", methods=['POST'])
+def create_member():
+    name = request.form['name']
+    age = request.form['age']
+    nationality = request.form['nationality']
+    mob_number = request.form['mob_number']
+    email = request.form['email']
+    
+    member = Member(name, age, nationality, mob_number, email)
+
+    member_repository.save(member)
+    return redirect('/members')
--- controllers/session_controller.py
+++ controllers/session_controller.py
@@ -16,3 +16,18 @@ def sessions():
 def description(id):
     session = session_repository.select_session(id)
     return render_template("sessions/description.html", session=session)
+
+@sessions_blueprint.route("/home", methods=['POST'])
+def create_session():
+    name = request.form['session_name']
+    tod = request.form['tod']
+    doy = request.form['doy']
+    length = request.form['length']
+    capacity = request.form['capacity']
+    description = request.form['description']
+    level = request.form['level']
+    
+    session = Session(name, tod, doy, length, capacity, description, level)
+
+    session_repository.save(session)
+    return redirect('/sessions')
